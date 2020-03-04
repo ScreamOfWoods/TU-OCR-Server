dd13570b79f0720a4b46773631695197e39b8f23
Atanas Shalaverov <ashalaverov@mm-sol.com>
2020-03-04 15:20:06 +0200

    Slight rework. The server can now accept a photo and save it to a desired folder

--- rest.py
+++ rest.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python3
 
 import base64
+import random
+import string
+import os
 from flask import Flask
 from flask import request
 
+
 IMAGE_DIR = '/mnt/win/TU_Projects/KZ/recv_imgs/'
+lang="eng"
 
 app = Flask(__name__)
 
-@app.route('/ocr', methods=['GET', 'POST'])
+@app.route('/ocr', methods=['POST'])
 def invoke_ocr():
     error = None
-    #TODO Get the parameters from the request
-    photo_str = request.form.get('photo')
-    #photo_name = request.form.get('photo_name')
+    #Get the parameters from the request
+    photo_str = request.get_data()
+    #print(photo_str)
+    lang = request.args.get('lang')
+
     #TODO spawn the ocr tool
-    print(photo_str)
+    #Save the photo data to the storage
+    filename = save_photo(photo_str)
+    print("OCR IN PROGRESS")
+    print(lang)
+    #print("Deleting photo from server")
+    #os.remove(filename) 
     return 'Hello!'
 
 
-def save_photo(photo_stream, photo_name):
+def save_photo(photo_stream):
     img = base64.b64decode(photo_stream)
-    filename = IMAGE_DIR + photo_name + ".png"
-    with open(filename, 'wb') as f:
-        f.write(img)
+    filename = IMAGE_DIR + random_string() + ".png"
+    with open(filename, 'wb') as file:
+        file.write(img)
+        file.close()
+    return filename
+
+
+def random_string(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
