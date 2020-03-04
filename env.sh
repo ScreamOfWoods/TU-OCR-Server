ef10aca2563fed2761b1b10560ec80cbff4e890c
Atanas Shalaverov <ashalaverov@mm-sol.com>
2020-03-05 01:43:43 +0200

    Removed the step that saves the data as an image to the storage and then reopens in

--- env.sh
+++ env.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 export TESSDATA_PREFIX=$HOME/tools/tesseract/tesseract/tessdata/
-
-#Mount windows data partirion
-sudo ntfs-3g /dev/sdb2 /mnt/win
-if [ $? != 0 ]; then
-    sudo umount /mnt/win
-    sudo ntfsfix /dev/sdb2
-    sudo ntfs-3g /dev/sdb2 /mnt/win
-fi
-
+export FLASK_APP=rest.py
--- rest.py
+++ rest.py
@@ -4,44 +4,32 @@ import base64
 import random
 import string
 import os
+import ocr
 from flask import Flask
 from flask import request
+from PIL import Image
+from io import BytesIO
 
-
-IMAGE_DIR = '/mnt/win/TU_Projects/KZ/recv_imgs/'
 lang="eng"
+recognized_text = ""
 
 app = Flask(__name__)
 
 @app.route('/ocr', methods=['POST'])
 def invoke_ocr():
-    error = None
     #Get the parameters from the request
     photo_str = request.get_data()
-    #print(photo_str)
     lang = request.args.get('lang')
 
-    #TODO spawn the ocr tool
-    #Save the photo data to the storage
-    filename = save_photo(photo_str)
+    #Load the image in memory
+    #spawn the ocr tool
+    img = Image.open(BytesIO(base64.b64decode(photo_str)))
     print("OCR IN PROGRESS")
-    print(lang)
-    #print("Deleting photo from server")
-    #os.remove(filename) 
-    return 'Hello!'
 
+    try:
+        recognized_text = ocr.extract_text(img, lang)
+    except Exception as e:
+        print(str(e))
+        return "Something happend. Cannot parse photo", 500
 
-def save_photo(photo_stream):
-    img = base64.b64decode(photo_stream)
-    filename = IMAGE_DIR + random_string() + ".png"
-    with open(filename, 'wb') as file:
-        file.write(img)
-        file.close()
-    return filename
-
-
-def random_string(stringLength=10):
-    """Generate a random string of fixed length """
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
+    return recognized_text;
